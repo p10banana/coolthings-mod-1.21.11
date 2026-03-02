65a136aeee87b2fc2acbe80087c0e658ab5a602f
p10banana <p10banana@gmail.com>
2026-03-02 20:08:36 -0300

    does this work

---
+++ src/main/java/net/p10banana/coolthings/CoolthingsClient.java
@@ -0,0 +1,10 @@
+package net.p10banana.coolthings;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoolthingsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/p10banana/coolthings/CoolthingsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.p10banana.coolthings;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CoolthingsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
